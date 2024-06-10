537db84326d78c9cec27fb675b83998482b7e6c1
Robert Jenkins <rjenkins8142@gmail.com>
2024-06-10 12:13:03 -0600

    configs

--- src/main.mjs
+++ src/main.mjs
@@ -4,50 +4,40 @@ import { Actor } from "apify";
 import playwright from "playwright";
 import { router } from "./router.mjs";
 import cli from "./parse_args.mjs";
-//import siteMap from './siteMap.js';
 
 async function main(argv) {
-  const otherOptions = {
+  const launchOptions = {
     headless: argv.headless,
-    args: ['--disable-web-security'],
+    args: ["--disable-web-security"],
   };
-  const proxyOptions = argv.proxy
-    ? {
-        proxy: {
-          server: argv.proxy,
-          username: argv.username,
-          password: argv.password,
-        },
-      }
-    : {};
-  const launchOptions = Object.assign(otherOptions, proxyOptions);
 
   var proxyConfiguration;
   var apifyProxy = false;
 
-  if (argv.proxy_urls.length == 1 && argv.proxy_urls[0] === 'apify') {
-    log.info('Using Apify proxy');
+  if (argv.proxyUrls.length == 1 && argv.proxyUrls[0] === "apify") {
+    log.info("Using Apify proxy");
     apifyProxy = true;
 
     await Actor.init();
 
     proxyConfiguration = await Actor.createProxyConfiguration({
-      groups: ['RESIDENTIAL'],
-      countryCode: 'US',
+      groups: ["RESIDENTIAL"],
+      countryCode: "US",
     });
-  } else if (argv.proxy_urls.length == 1 && argv.proxy_urls[0] === 'skip') {
-    log.info('Skipping proxy');
-    proxyConfiguration = undefined
+  } else if (argv.proxyUrls.length == 1 && argv.proxyUrls[0] === "skip") {
+    log.info("Skipping proxy");
+    proxyConfiguration = undefined;
   } else {
-    log.info('Using custom proxy');
+    log.info("Using custom proxy");
     proxyConfiguration = new ProxyConfiguration({
-      proxyUrls: argv.proxy_urls,
+      proxyUrls: argv.proxyUrls,
     });
   }
 
-  log.debug('Starting crawler')
+  log.debug("Starting crawler");
 
-  const launcher = playwright[argv.browser === 'chrome' ? 'chromium' : argv.browser];
+  const launcher =
+    playwright[argv.browser === "chrome" ? "chromium" : argv.browser];
 
   const crawler = new PlaywrightCrawler({
     maxRequestsPerCrawl: 100,
@@ -59,13 +49,13 @@ async function main(argv) {
     },
     launchContext: {
       launcher: launcher,
-      useChrome: argv.browser === 'chrome',
+      useChrome: argv.browser === "chrome",
       launchOptions: launchOptions,
     },
     requestHandler: router,
   });
 
-  await crawler.addRequests([argv.website]);
+  await crawler.addRequests([argv.siteConfig.startUrl]);
   await crawler.run();
 
   if (apifyProxy) {
@@ -74,6 +64,6 @@ async function main(argv) {
 }
 
 // Get command line options
-const options = cli.parse_args(process.argv);
+const options = cli.parseArgs(process.argv);
 
 main(options);
--- src/parse_args.mjs
+++ src/parse_args.mjs
@@ -1,14 +1,39 @@
+import fs from "fs";
+import path from "path";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
-function parse_args(args) {
-  const options = yargs(hideBin(args))
-    .option("proxy_urls", {
+function parseArgs(args) {
+  // Read from sites directory, and get a list of websites.
+  const configDir = "./configs"; // Directory containing the config files
+
+  // Read the config files and extract the site names and configs
+  const configFiles = fs
+    .readdirSync(configDir)
+    .filter((file) => file.endsWith(".json"));
+  const websites = {};
+  var websiteDefault = "";
+  configFiles.forEach((file) => {
+    const configPath = path.join(configDir, file);
+    const config = JSON.parse(fs.readFileSync(configPath, "utf8"));
+    if (config.default) {
+      websiteDefault = config.site;
+    }
+    websites[config.site] = config;
+  });
+  websites["other"] = { site: "other", startUrl: "" };
+
+  var options = yargs(hideBin(args))
+    .parserConfiguration({
+      "camel-case-expansion": true,
+    })
+    .option("proxy-urls", {
       alias: "p",
       describe: "Comma separated list of proxies, or 'skip', or 'apify'.",
       type: "string",
-      coerce: (arg) => arg.split(','),
-      demandOption: "Please provide a list of proxy URLs or 'skip' to run without a proxy, or 'apify' to use Apify proxy",
+      coerce: (arg) => arg.split(","),
+      demandOption:
+        "Please provide a list of proxy URLs or 'skip' to run without a proxy, or 'apify' to use Apify proxy",
     })
     .option("headless", {
       alias: "H",
@@ -25,13 +50,35 @@ function parse_args(args) {
     })
     .option("website", {
       alias: "w",
-      describe: "Website URL to scrape products from",
+      describe:
+        "Website to scrape products from, or full URL if not already mapped.",
       type: "string",
-      default: "https://www.radwell.com/en-US/",
-    })
-    .argv;
+      choices: Object.keys(websites),
+      default: websiteDefault,
+      coerce: (arg) => {
+        var site = websites[arg];
+        if (site && arg != "other") {
+          return arg;
+        } else if (arg.startsWith("http://") || arg.startsWith("https://")) {
+          // If the provided value is a valid URL, return it as is
+          websites["other"].startUrl = arg;
+          return "other";
+        } else {
+          // If the provided value is neither a predefined website name nor a valid URL, throw an error
+          throw new Error("Invalid website name or URL");
+        }
+      },
+    }).argv;
+
+  // Get the selected website's config object
+  const websiteConfig = websites[options.website];
+
+  // Assign the website config object to the argv object
+  if (websiteConfig) {
+    Object.assign(options, { siteConfig: websiteConfig });
+  }
 
   return options;
 }
 
-export default { parse_args };
+export default { parseArgs };
